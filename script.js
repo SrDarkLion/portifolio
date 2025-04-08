053eed9565e5e52158713fab9c0db684f6bdd1c6
Davi Quinino <daviquininooficial@gmail.com>
2025-04-08 17:40:03 -0300

    corrigi a responsividade

---
+++ script.js
@@ -0,0 +1,25 @@
+const menuBurguer = document.querySelector(".menu-burguer");
+const menu = document.querySelector(".menu");
+const links = document.querySelectorAll(".menu-link");
+const body = document.body;
+
+menuBurguer.addEventListener("click", () => {
+  menuBurguer.classList.toggle("ativo");
+  menu.classList.toggle("ativo");
+  body.classList.toggle("scroll-block");
+});
+
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    menuBurguer.classList.remove("ativo");
+    menu.classList.remove("ativo");
+    body.classList.remove("scroll-block");
+  });
+});
+
+const flipContainer = document.querySelector('.flip-container');
+const flipCard = document.querySelector('.flip-card');
+
+flipContainer.addEventListener('click', () => {
+    flipCard.classList.toggle('virar');
+});
